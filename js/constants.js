01875f89d17e04e8456d9a2081578fb07018136a
SmileyTheSmile <34063023+SmileyTheSmile@users.noreply.github.com>
2020-07-08 15:22:43 +0300

    Commit 1
    Первый коммит. Реализована рисовка заднего плана, полей для игры и добавления на них кораблей.

---
+++ js/constants.js
@@ -0,0 +1,11 @@
+const ALPHABET = " АБВГДЕЁЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+const CANVAS_WIDTH = 500;
+const CANVAS_HEIGHT = 500;
+const CELL_SIZE = 23;
+const BOARD_CELL_SIZE = 30;
+const BOARD_COL_NUM = 10;
+const BOARD_ROW_NUM = 10;
+const RED_LINE_OFFSET = 73;
+const PLAYER_BOARD_OFFSET_X = 50;
+const PLAYER_BOARD_OFFSET_Y = 50 + RED_LINE_OFFSET;
+
